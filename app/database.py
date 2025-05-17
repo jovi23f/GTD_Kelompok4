7cc924531965b48fe68d3f3cffc434214a6d3d79
Jovita F. K. <100178380+jovi23f@users.noreply.github.com>
2025-05-17 19:01:03 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from databases import Database
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://ro_user:ro_password@localhost:5432/terrorism_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://your_username:your_password@localhost:5432/terrorism_db")
 
 database = Database(DATABASE_URL)
